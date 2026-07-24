247b06db11464eff4c8aff97f8a6175b6166f5a8
dopaemon <polarisdp@gmail.com>
2026-07-24 21:30:27 +0700

    fix(web): serve dashboard SPA routes

--- internal/web/web.go
+++ internal/web/web.go
@@ -2,10 +2,22 @@ package web
 
 import (
 	"embed"
+	"io/fs"
 	"net/http"
 )
 
 //go:embed index.html
 var files embed.FS
 
-func Handler() http.Handler { return http.FileServer(http.FS(files)) }
+func Handler() http.Handler {
+	static := http.FileServer(http.FS(files))
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" || r.URL.Path == "/dashboard" || r.URL.Path == "/dashboard/" || r.URL.Path == "/login" {
+			r.URL.Path = "/index.html"
+		}
+		if _, err := fs.Stat(files, r.URL.Path[1:]); err != nil {
+			r.URL.Path = "/index.html"
+		}
+		static.ServeHTTP(w, r)
+	})
+}
